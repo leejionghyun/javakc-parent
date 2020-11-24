3204f7b390a4653d4a0a4f0f87397f8e5e0eab98
javakc_global <javakc_global@javakc.com>
2020-11-24 13:45:47 +0800

    这是我项目的第一次提交

---
+++ service/service-pms/src/main/java/com/javakc/pms/dispord/dao/DispOrdDao.java
@@ -0,0 +1,11 @@
+package com.javakc.pms.dispord.dao;
+
+import com.javakc.commonutils.jpa.base.dao.BaseDao;
+import com.javakc.pms.dispord.entity.DispOrd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DispOrdDao extends BaseDao<DispOrd,String> {
+
+}
